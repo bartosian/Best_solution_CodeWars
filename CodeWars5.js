cbf22c8ec7ca8751e92b476d9ae94ec2eb504cd5
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-28 14:18:09 -0400

    Added solution

--- CodeWars5.js
+++ CodeWars5.js
@@ -9,3 +9,11 @@ function lastDayIsFriday(initialYear, endYear = initialYear) {
 }
 
 // link on codeWars https://www.codewars.com/kata/5a0d6d8c6975982b5b000383/solutions/javascript
+
+function sumArray(array) {
+    return Array.isArray(array) && array.length > 1
+        ? array.reduce((s, n) => s + n, 0) - Math.min(...array) - Math.max(...array)
+        : 0;
+}
+
+// link on codeWars https://www.codewars.com/kata/576b93db1129fcf2200001e6/solutions/javascript
