9efcafb75ceaaaa2e2952e45682981eb8b9e13f2
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-30 10:34:24 -0400

    Extract domain solved

--- CodeWars5.js
+++ CodeWars5.js
@@ -276,7 +276,14 @@ function sumOfDifferences(arr) {
 }
 
 
+function domainName(url) {
 
+    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if ( match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0 ) return match[2].split(".")[0];
+}
+
+var one = domainName("http://github.com/carbonfive/raygun");
+var two = domainName("http://www.zombie-bites.com");
 
 
 
