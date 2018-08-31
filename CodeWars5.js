8b21f07c7366216a05c6956dd4f599946b8d338f
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-31 12:58:36 -0400

    Solved one more

--- CodeWars5.js
+++ CodeWars5.js
@@ -464,6 +464,13 @@ function replace(str) {
 }
 
 
+// fn(['super','bow','bowl','tar','get','book','let'], "superbowl")      =>   ['super','bowl',   [0,2]]
+// fn(['bow','crystal','organic','ally','rain','line'], "crystalline")   =>   ['crystal','line', [1,5]]
+// fn(['bow','crystal','organic','ally','rain','line'], "rainbow")       =>   ['bow','rain',     [4,0]]
+// fn(['bow','crystal','organic','ally','rain','line'], "organically")   =>   ['organic','ally', [2,3]]
+// fn(['top','main','tree','ally','fin','line'], "mainline")             =>   ['main','line',    [1,5]]
+// fn(['top','main','tree','ally','fin','line'], "treetop")              =>   ['top','tree',     [2,0]]
+
 
 
 
