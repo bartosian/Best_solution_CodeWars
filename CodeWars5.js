67407f24ad43aa7e1539503d7803b8bf1d40803b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-04 21:34:10 -0400

    Solved task

--- CodeWars5.js
+++ CodeWars5.js
@@ -646,7 +646,22 @@ function spinWords(str) {
 
 }
 
+function multiply(num) {
 
+    if(num <= 0) return 0;
+    let sum = 0;
+
+
+    for(let i = num - 1; i > 0; i--) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+
+    }
+
+    console.log(sum);
+
+}
 
 
 
