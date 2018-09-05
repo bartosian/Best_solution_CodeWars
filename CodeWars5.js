01ed5793f6c4273ae51686532015dd95a913a0b1
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-04 20:45:31 -0400

    Solved array_diff task

--- CodeWars5.js
+++ CodeWars5.js
@@ -587,5 +587,22 @@ function multiplayBig(a, b) {
 }
 
 
+function array_diff(arr1, arr2) {
+
+    for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr2.length; j++) {
+
+            if(arr1[i] === arr2[j]) {
+                arr1.splice(i, 1);
+                i--;
+            }
+
+        }
+
+    }
+
+return arr1;
+
+}
 
 
